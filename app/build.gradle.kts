f0b56a692647227cc8d585275f6e310e45e2605c
AGT Studio <tan.ahmet.at@gmail.com>
2026-09-16 01:53:55 +0300

    Bump Builder version for self-package name fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "com.agtstudio.zipapk"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0.0"
+        versionCode = 4
+        versionName = "3.1.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
